6eecb59e7f359c723abeb141dd287f97c56d9236
Subha30.coder <subhankarmaitra2003@gmail.com>
2025-07-06 18:22:19 +0530

    Add Function Tutorial

---
+++ Function_tutorial_ncert/tent.py
@@ -0,0 +1,39 @@
+"""Make prog to take dimensioins as
+input from user & output length of fabric
+required and total payable amount"""
+
+#Taking input from the user---
+
+print("Enter dimension of Tent(in meter):---")
+h=float(input("Enter the height of tent:"))
+r = float(input("Enter the radius of the tent:"))
+s=float(input("Enter the slant height of tent:"))
+
+
+#define Constant Pi=3.14
+pi=3.14
+
+#Curved Surface Area of conical part
+csa_cone=pi*r*s
+
+#Curved Surface Area of cyllinder part
+csa_cyllinder=2*pi*r*h
+
+#Total area of canvas--
+Total_Area=csa_cone+csa_cyllinder
+
+print("Total Area of canvas is- ",Total_Area)
+
+#Dimension of fabric
+w=float(input("Enter width of fabric:"))
+l=(Total_Area/w)
+print("Length of the fabric required:",l)
+p=int(input("Enter price per meter lentgth of fabric:"))
+Total_cost=(l*p)+118/100
+print("The total cost to be paid(with 18 1percent GST):",Total_cost)
+
+
+"""Another way of doing so in organised Modular programming way using Functions"""
+
+
+
