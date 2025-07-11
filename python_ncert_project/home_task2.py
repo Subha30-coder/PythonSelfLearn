0d65540dce9369cb9f43451179d95db0ba5056d1
Subha30.coder <subhankarmaitra2003@gmail.com>
2025-07-11 21:35:30 +0530

    uncommented  main code

--- python_ncert_project/home_task2.py
+++ python_ncert_project/home_task2.py
@@ -1,45 +1,45 @@
 """Make some pattern row(|) by column(-)"""  
 
 """for that specific pattern of  this type """  
-# for row in range(1,6):              # *
-#     for column in range(row):       # * *
-#          print("*",end=" ")         # * * *
-#     print("\n")                     # * * * *                                                                       
-                                      # * * * * * 
+for row in range(1,6):              # *
+    for column in range(row):       # * *
+         print("*",end=" ")         # * * *
+    print("\n")                     # * * * *                                                                       
+                                      * * * * * 
                                       
-# # for any general pattern as input(n)  of this type 
-# n=int(input("enter value of n :"))
-# for row in range(1,n+1):
-#     for column in range(row):
-#         print("*",end=" ")
-#     print("\n")
+# for any general pattern as input(n)  of this type 
+n=int(input("enter value of n :"))
+for row in range(1,n+1):
+    for column in range(row):
+        print("*",end=" ")
+    print("\n")
 
 
 """ for that specific pattern of  this type"""
-# for row in range(1,6):  #row srart from 1,as set up                      # 1 2 3 4 5
-#     for column in range(6-row):   # column starts from 0, by default     # 1 2 3 4
-#         print(column+1,end=" ")                                          # 1 2 3
-#     print("\n")                                                          # 1 2
-#                                                                          # 1
+for row in range(1,6):  #row srart from 1,as set up                      # 1 2 3 4 5
+    for column in range(6-row):   # column starts from 0, by default     # 1 2 3 4
+        print(column+1,end=" ")                                          # 1 2 3
+    print("\n")                                                          # 1 2
+                                                                         # 1
            
-# # for any general pattern as input(n)  of this type 
-# n=int(input("enter the value n :"))
-# for row in range(1,n+1):
-#     for column in range((n+1)-row):
-#         print(column+1,end=" ")
-#     print("\n")     
+# for any general pattern as input(n)  of this type 
+n=int(input("enter the value n :"))
+for row in range(1,n+1):
+    for column in range((n+1)-row):
+        print(column+1,end=" ")
+    print("\n")     
            
 
 """ for that specific pattern of  this type"""                                 #A
-# for row in range(1,6):           #row range start from 1,as set                #A B
-#     for column in range(row):     #column range starts from 0,by default       #A B C
-#         print( chr(column+65),end=" ")  # chr()-->ASCIcode reader,chr(65)=A    #A B C D
-#     print("\n")                                                                #A B C D E
+for row in range(1,6):           #row range start from 1,as set                #A B
+    for column in range(row):     #column range starts from 0,by default       #A B C
+        print( chr(column+65),end=" ")  # chr()-->ASCIcode reader,chr(65)=A    #A B C D
+    print("\n")                                                                #A B C D E
 
-# # for any general pattern as input(n)  of this type
-# n=int(input("put the value n:"))
-# for row in range(1,n+1):
-#     for column in range(row):
-#         print(chr(column+65),end=" ")
-#     print("\n")
+# for any general pattern as input(n)  of this type
+n=int(input("put the value n:"))
+for row in range(1,n+1):
+    for column in range(row):
+        print(chr(column+65),end=" ")
+    print("\n")
 
--- python_ncert_project/home_task3.py
+++ python_ncert_project/home_task3.py
@@ -2,54 +2,54 @@
 of following series"""
 
 """ 1 + x + x² + x³ + x⁴ +..... + xⁿ  """
-# n=int(input("Enter value of n:"))
-# x=int(input("Enter value of x:"))
-# total=1
-# for i in range(1,n+1):
-#     total+=pow(x,i)
-# print("The sum of the series :",total)
+n=int(input("Enter value of n:"))
+x=int(input("Enter value of x:"))
+total=1
+for i in range(1,n+1):
+    total+=pow(x,i)
+print("The sum of the series :",total)
 
 
 # """ 1 - x + x² - x³ + x⁴ - ...... - xⁿ """   # 1 + (-x)¹ + (-x)² + (-x)³ + (-x)⁴ +...
 
-# n=int(input("Enter value of n:"))
-# x=int(input("Enter value of x:"))
-# total=1
-# for i in range(1,n+1):
-#     total+=pow(-x,i)
-# print("The sum of the series :",total)
+n=int(input("Enter value of n:"))
+x=int(input("Enter value of x:"))
+total=1
+for i in range(1,n+1):
+    total+=pow(-x,i)
+print("The sum of the series :",total)
 
 
 
 """ x - x²/2 + x³/3 - x⁴/4 +...... xⁿ/n """ # -{ -x + x²/2 - x³/3 + x⁴/4...}  ==> # - { (-x) + (-x)²/2 + (-x)³/3 +......}
                                                 
-# n=int(input("Enter value of n:"))
-# x=int(input("Enter value of x:"))
-# total=0
-# for i in range(1,n+1):
-#     total+= - pow(-x,i)/i
-# print("The sum of the series :",total)
+n=int(input("Enter value of n:"))
+x=int(input("Enter value of x:"))
+total=0
+for i in range(1,n+1):
+    total+= - pow(-x,i)/i
+print("The sum of the series :",total)
 
 
 
 """ x + x²/2! - x³/3! + x⁴/4! - ....xⁿ/n! """
 
-# import math
+import math
 
-# # Input values
-# x = float(input("Enter the value of x: "))
-# n = int(input("Enter the number of terms n: "))
+# Input values
+x = float(input("Enter the value of x: "))
+n = int(input("Enter the number of terms n: "))
 
-# # Initialize sum
-# sum_series = 0
+# Initialize sum
+sum_series = 0
 
-# # Loop through terms from 1 to n
-# for i in range(1, n + 1):
-#     sign = (-1) ** (i + 1)  # Alternating sign: + - + - ...
-#     term = sign * (x ** i) / math.factorial(i)
-#     sum_series += term
+# Loop through terms from 1 to n
+for i in range(1, n + 1):
+    sign = (-1) ** (i + 1)  # Alternating sign: + - + - ...
+    term = sign * (x ** i) / math.factorial(i)
+    sum_series += term
 
-# # Print the result
-# print(f"The sum of the series is: {sum_series}")
+# Print the result
+print(f"The sum of the series is: {sum_series}")
 
 
