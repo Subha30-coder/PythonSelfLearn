a3b97bf644355854d23f7f9f173073f5bfd8c52e
Subha30.coder <subhankarmaitra2003@gmail.com>
2025-07-13 20:13:05 +0530

    some add on task

--- Function_tutorial_ncert/tent2.py
+++ Function_tutorial_ncert/tent2.py
@@ -4,28 +4,35 @@ Functions to enhance readibility and reuseability"""
 
 from math import pi
 
-#Curved surface area of cyllinder 
+
 def cyllinder(h,r):
+    "Curved surface area of cyllinder "
     area_cyl=2*pi*r*h
     return area_cyl
-#Curved surface area of cone
+
 def cone(l,r):
+    "Curved surface area of cone"
     area_con=pi*r*l
     return area_con
-#Length of Fabric
+
 def fabric_len(total_area,width_fabric):
+    "Length of Fabric"
     length=total_area/width_fabric
     return length
-#Find cost exclusive of tax 
+ 
 def cost(length_fabric,rate):
+    "Find cost exclusive of tax"
     total_cost=length_fabric*rate
     return total_cost
-#Net amount inclusive of tax
+
 def net_pay(cost):
+    "Net amount inclusive of tax"
     tax=0.18*cost 
     Net_price=cost+tax
     return Net_price
 
+## The fuction won't execute by itself , 
+# we need to call it whenever needed in code.
 
 
 
@@ -35,19 +42,19 @@ r = float(input("Enter the radius of the tent:"))
 s=float(input("Enter the slant height of tent:"))
 print("\n\n\n")
 
-csa_cone=cone(s,r)
-csa_cyllinder=cyllinder(h,r)
-print(".............................")
+csa_cone=cone(s,r)                    #here we call the function cone that had made before
+csa_cyllinder=cyllinder(h,r)            # here we call the function cyllinder
+print(".............................") 
 Total_area=csa_cone+csa_cyllinder
 print("Total area:",Total_area)
 
-length_of_fabric=fabric_len(Total_area,20)
+length_of_fabric=fabric_len(Total_area,20)    #here we call the function  fabric_length
 print("Length of fabric req:--",length_of_fabric)
 
 rate=float(input("Please Enter the rate of the fabric :--"))
-Total_cost_ex_Tax=cost(length_of_fabric,rate)
+Total_cost_ex_Tax=cost(length_of_fabric,rate)                 # we call the function cost
 
-Total_cost_in_Tax=net_pay(Total_cost_ex_Tax)
+Total_cost_in_Tax=net_pay(Total_cost_ex_Tax)                   #we call the fuction net_pay
 print("Price exclusive of tax:",Total_cost_ex_Tax)
 print("price inclusive of tax:",Total_cost_in_Tax)
 
---
+++ Function_tutorial_ncert/user_def_func.py
@@ -0,0 +1,17 @@
+"""Creation of userdefined Function"""
+
+# def welcome_messege():
+#     'Funtion to print a welcome messege'    #Docstring describing the function
+#     print("Welcome to wonder world")   
+    
+    
+# welcome_messege()  ## The fuction won't execute by itself , we need to call it whenever needed in code 
+# print("Def of Function: ",welcome_messege.__doc__)  # we call the docstring here
+
+
+# def Add (a,b):            #presenting with argument
+#     c=a+b
+#     return c
+
+# c=Add(2,5)
+# print(c)
